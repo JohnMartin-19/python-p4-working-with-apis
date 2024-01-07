7694d45208cfc0042aee6283524f67b60c4ef6dc
John <johnnkonge2020@gmail.com>
2024-01-07 13:22:27 +0300

    added a method to get program list

--- lib/nyc_api.py
+++ lib/nyc_api.py
@@ -3,12 +3,20 @@ import requests
 import json
 
 class GetPrograms:
-
+  
+  def program_school(self):
+    programs_list = []
+    programs = json.loads(self.get_programs())
+    for program in programs:
+        programs_list.append(program['agency'])
+    return programs_list
+  
   def get_programs(self):
     URL = "http://data.cityofnewyork.us/resource/uvks-tn5n.json"
 
     response = requests.get(URL)
     return response.content
 
+  
 programs = GetPrograms().get_programs()
 print(programs)
